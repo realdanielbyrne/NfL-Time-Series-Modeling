46f93f43418256804d30e722f60bfd8eaa88b1ff
Daniel Byrne <realdanielbyrne@icloud.com>
2022-01-04 23:57:51 -0600

    updated data and added sktime models

--- skmodels.py
+++ skmodels.py
@@ -1,5 +1,5 @@
 # %%
-from sktime.forecasting.naive import NaiveForecaster
+# imports
 from sktime.forecasting.bats import BATS
 from sktime.forecasting.tbats import TBATS
 from sktime.forecasting.arima import AutoARIMA
@@ -7,90 +7,96 @@ from sktime.forecasting.exp_smoothing import ExponentialSmoothing
 from sktime.forecasting.trend import PolynomialTrendForecaster
 from sktime.utils.plotting import plot_series
 import pandas as pd
-
-
-# teams conpared
-teams = ['car', 'nor']
-
-# historical data range [start,stop]
-# end points are inclusive
-years = ['2018', '2021']
-
-# forecasting horizon
-fh = 1
-
-
-def read_data():
-    teamfile = './stats/combinedstats/{team}_{start}_{stop}.csv'.format(team=teams[0],start=years[0],stop=years[1])
-    teamdf = pd.read_csv(teamfile)
-
-    oppfile = './stats/combinedstats/{opp}_{start}_{stop}.csv'.format(opp=teams[1],start=years[1],stop=years[1])
-    oppdf = pd.read_csv(oppfile)
-    return [teamdf, oppdf]
-
-
-
-# step 3: specifying the forecasting algorithm
-models = {
-    "bats":BATS(
-        use_box_cox=False,
-        use_trend=True,
-        use_damped_trend=True,
-        use_arma_errors=True,
-        sp=12),
-
-    "tbats":TBATS(
-        use_box_cox=False,
-        use_trend=True,
-        use_damped_trend=True,
-        use_arma_errors=True,
-        sp=12),
-
-    "aarima":AutoARIMA(sp=12, d=0, max_p=2, max_q=2, suppress_warnings=True),
-    "exp":ExponentialSmoothing(trend='add', seasonal='additive', sp=12),
-    "poly":PolynomialTrendForecaster(degree=1)
-}
+import argparse
 
 #%%
-# Team Predictions
+def read_data(teams, years):
+    data = []
+    for team in teams:
+        teamfile = './stats/combinedstats/{team}_{start}_{stop}.csv'.format(team=team,start=years[0],stop=years[1])
+        df = pd.read_csv(teamfile)
+        df['team']= team
+        data.append(df)
 
-def get_teams():
-    predictions = pd.DataFrame()
-    for t in teams:
-        exo = teamdf[['Off_Totyd','Off_TO','Def_TO','PassY/A','PassCmp','RushAtt', 'RushYds', 'RushY/A', 'FGM','FGA','Pnt','PntYds']].copy() 
-        target = teamdf['TmScore']
-        holdout = teamdf['TmScore']
+    return data
+
+#%%
+def get_predictions(teamdata, models, fh):
+    
+    cols = list(models.keys())
+    cols.append('team')
+    predictions = pd.DataFrame(columns=cols)
+    for d in teamdata:
+        exo = d[['Off_Totyd','Off_TO','Def_TO','PassY/A', 'PassCmp', 'RushAtt',
+                    'RushYds', 'RushY/A', 'FGM', 'FGA', 'Pnt', 'PntYds']].copy() 
+        target = d['TmScore']
         exo_mu = pd.DataFrame(exo.mean(axis=0)).transpose()
         
-        for m in models:
-            models[m].fit(target, X=exo)
-            predictions[m]= models[m].predict(fh, X=exo_mu)
-            predictions['team'] = t
-
-
-#%%
-# Opp Predictions
-oppexo = oppdf[['Off_Totyd','Off_TO','Def_TO','PassY/A','PassCmp','RushAtt', 'RushYds', 'RushY/A', 'FGM','FGA','Pnt','PntYds']].copy() 
-target = oppdf['TmScore']
-holdout = oppdf['TmScore']
-oppexo_mu = pd.DataFrame(oppexo.mean(axis=0)).transpose()
-
-opppredictions = pd.DataFrame()
-for m in models:
-    models[m].fit(target, X=oppexo)
-    opppredictions[m]= models[m].predict(fh, X=oppexo_mu)
-    opppredictions['team'] = opp  
-
-opppredictions
-
-#%%
-# Spreads
-
-
-
-
+        prediction = pd.Series(dtype='float64')
+        for key, model in models.items():
+            model.fit(target, X=exo)
+            result = model.predict(fh, X = exo_mu)
+            prediction[key] = result.iloc[0]
+            
+        prediction['team'] = d.loc[0,'team']
+        predictions.append(prediction,ignore_index=True, sort=False )
+        predictions = predictions.append(prediction, ignore_index=True, sort=False )
+    predictions.set_index('team', inplace=True)
+        
+    return predictions
+       
+def get_matchups():
+    pass 
 
 # %%
-ou = predictions.to_numpy() + opppredictions.to_numpy()
-ou
+if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser(description='NFL spread Predictor.')
+    parser.add_argument('-b','--start',
+                        action='store',
+                        type=int,
+                        default=2018,
+                        required=False,
+                        help="Historical data range start year.")
+
+    parser.add_argument('-e','--end',
+                        type=int,
+                        default=2021,
+                        required=False,
+                        help="Historical data range end year.")
+    
+    parser.add_argument('-f','--fh',
+                        type=int,
+                        default=1,
+                        required=False,
+                        help="Forecast horizon.")
+    
+    args = parser.parse_args("")
+
+    # teams
+    teams = ['crd', 'atl', 'rav', 'buf', 'car', 'chi', 'cin', 'cle', 'dal',
+         'den', 'det', 'gnb', 'htx', 'clt', 'jax', 'kan', 'rai', 'sdg',
+         'ram', 'mia', 'min', 'nwe', 'nor', 'nyj', 'phi', 'pit', 'sfo',
+         'sea', 'tam', 'oti', 'was']
+
+    # historical data range [start,stop]
+    # end points are inclusive
+
+    # step 3: specifying the forecasting algorithm
+    models = {
+
+        "tbats":TBATS(
+            use_box_cox=False,
+            use_trend=True,
+            use_damped_trend=False,
+            use_arma_errors=True),
+
+        "aarima":AutoARIMA(sp=12, d=0, max_p=4, max_q=2, suppress_warnings=True),
+        "exp":ExponentialSmoothing(trend='add', seasonal='additive', sp=12),
+        "poly":PolynomialTrendForecaster(degree=3)
+    }
+
+    teamdata = read_data(teams, [args.start,args.end])
+    preds = get_predictions(teamdata, models, args.fh)
+    
+# %%
